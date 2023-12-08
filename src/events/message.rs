b2ffa4f0d43586a836790c050e69371581760939
Hayden1321 <hayden1321@hayden1321.com>
2023-12-08 23:53:36 +0000

    the rest...

--- src/events/message.rs
+++ src/events/message.rs
@@ -1,43 +1,152 @@
-use std::io::Cursor;
-
 use rusty_tesseract::{Args, Image};
-use serenity::client::Context;
+use serenity::{client::Context, all::ReactionType};
 use regex::Regex;
-use reqwest;
+use url::Url;
+use leptess::LepTess;
+
+use crate::{Group, Keyword, Paste};
 
 pub async fn message(handler: &crate::Handler, ctx: Context, msg: serenity::model::channel::Message) {
-	let url: Result<&str, _> = if &msg.attachments.len() > &0 {
-		Ok(&msg.attachments[0].proxy_url.as_str())
-	} else if &msg.content.len() > &0 {
-		let reg = Regex::new(r"(?P<url>https?://.*)").expect("Failed to compile regex");
-
-		let cap = reg.captures(&msg.content).expect("Failed to capture regex");
-
-		Ok(cap.name("url").unwrap().as_str())
-		
-	} else { 
+	let url = if msg.attachments.len() > 0 {
+		Ok(msg.attachments[0].proxy_url.as_str())
+	} else if msg.content.len() > 0 {
+		if let Ok(reg) = Regex::new(r"(?P<url>https?://.*)") {
+			if let Some(cap) = reg.captures(&msg.content) {
+				Ok(cap.name("url").unwrap().as_str())
+			} else {
+				Err(())
+			}
+		} else {
+			Err(())
+		}
+	} else {
 		Err(())
 	};
 
+
+	let groups = handler.cfg.groups.iter().filter(|x| x.channels.contains(&msg.channel_id.to_string())).collect::<Vec<&Group>>();
+	let group = *groups.get(0).unwrap();
+
 	if url.is_ok() {
 		let url = url.expect("Failed to get URL, idk why tho.");
-		let body = reqwest::get(url).await.unwrap().bytes().await.unwrap();
 
-		let image = image::load_from_memory(&body).unwrap();
+		let request = group.parsing.paste.iter().filter(|x| x.domain == Url::parse(url).unwrap().domain().unwrap()).collect::<Vec<&Paste>>();
+		if request.get(0).is_some() {
+			let paste = *request.get(0).unwrap();
+			let mut paste_url = url.to_string();
+
+			if !url.contains("raw") {
+				paste_url = paste.format.replace("&filename&", Url::parse(url).unwrap().path().trim_start_matches("/"));
+			}
+
+			let request = reqwest::get(paste_url).await.unwrap();
+			if request.headers().get("Content-Type").expect("Failed to get content type").to_str().unwrap().contains("text/plain") {
+				let text = request.text().await.unwrap();
+
+				let match_type = match_text(text, group.clone()).await;
+
+				if match_type.is_some() {
+					let match_type = match_type.unwrap();
 		
-		let default_args = Args::default();
+					match match_type {
+						MessageMatchType::Keyword(keyword) => {
+							msg.reply(&ctx.http, keyword.response.clone()).await.unwrap();
+							msg.react(&ctx.http, ReactionType::Unicode(keyword.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+						},
+						MessageMatchType::Regex(regex) => {
+							msg.reply(&ctx.http, regex.response.clone()).await.unwrap();
+							msg.react(&ctx.http, ReactionType::Unicode(regex.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+						}
+					}
+				}
+			} else {
+				msg.reply(&ctx.http, "Failed to get paste, invalid content type.").await.unwrap();
+			}
+
+			return			
+		}
+		
+		
+		let request = reqwest::get(url).await.unwrap();
+
+	  if request.headers().get("Content-Type").expect("Failed to get content type").to_str().unwrap().contains("image/jpeg") || request.headers().get("Content-Type").expect("Failed to get content type").to_str().unwrap().contains("image/png") {
+			let image = image::load_from_memory(&request.bytes().await.unwrap()).unwrap();
+			
+			let mut tesseract = LepTess::new(Some("./tessdata"), "eng").unwrap();
+		
+			tesseract.set_image_from_mem(image.as_bytes()).expect("Failed to set image from memory");
+
+			let output = tesseract.get_utf8_text().unwrap();
+
+			let match_type = match_text(output, group.clone()).await;
+
+			if match_type.is_some() {
+				let match_type = match_type.unwrap();
 	
-		let output = rusty_tesseract::image_to_string(&Image::from_dynamic_image(&image)
-		.unwrap(), &default_args).unwrap();
-	
-		println!("{:?}", output);
+				match match_type {
+					MessageMatchType::Keyword(keyword) => {
+						msg.reply(&ctx.http, keyword.response.clone()).await.unwrap();
+						msg.react(&ctx.http, ReactionType::Unicode(keyword.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+					},
+					MessageMatchType::Regex(regex) => {
+						msg.reply(&ctx.http, regex.response.clone()).await.unwrap();
+						msg.react(&ctx.http, ReactionType::Unicode(regex.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+					}
+				}
+			}
+		}
+	} else {
+		let message = msg.content.clone();
+
+		if message.len() == 0 { return }
+
+		let match_type = match_text(message, group.clone()).await;
+
+		if match_type.is_some() {
+			let match_type = match_type.unwrap();
+
+			match match_type {
+				MessageMatchType::Keyword(keyword) => {
+					msg.reply(&ctx.http, keyword.response.clone()).await.unwrap();
+					msg.react(&ctx.http, ReactionType::Unicode(keyword.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+				},
+				MessageMatchType::Regex(regex) => {
+					msg.reply(&ctx.http, regex.response.clone()).await.unwrap();
+					msg.react(&ctx.http, ReactionType::Unicode(regex.reaction.unwrap_or("👀".to_string()))).await.unwrap();
+				}
+			}
+		}
+	}
+
+	//TODO: Get OCR to read image, return. (CHANGE FOR NATIVE VERSION)
+}
+
+enum MessageMatchType {
+	Keyword(Keyword),
+	Regex(crate::Regex)
+}
+
+async fn match_text(text: String, group: Group) -> Option<MessageMatchType> {
+	for x in group.regex.iter() {
+		let reg = Regex::new(&x.pattern).expect("Failed to compile regex");
+
+		let cap = match reg.captures(&text) {
+			Some(x) => x,
+			None => continue
+		};
+
+		if cap.get(0).is_some() {
+			return Some(MessageMatchType::Regex(x.clone()))
+		}
+	}
+
+
+	let keywords = group.keywords.iter().filter(|x| text.trim().to_lowercase().split(" ").collect::<Vec<&str>>().contains(&x.keyword.as_str())).collect::<Vec<&Keyword>>();
+	if keywords.get(0).is_some() {
+		let keyword = *keywords.get(0).unwrap();
+
+		return Some(MessageMatchType::Keyword(keyword.clone()))
 	}
 	
-	// println!("URL: {}", cap.name("url").unwrap().as_str());
-	// if let Some(url) =  {
-		
-	// };
-
-	//TODO: Get OCR to read image, return.
-	//TODO: Check against config (add support for pastebins and regular text), return result.
+	None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,54 @@
-use std::env;
+use std::{env, fs::File, io::BufReader};
 
 use dotenv;
+use serde::Deserialize;
 use serenity::{Client, client::{EventHandler, Context}, async_trait, all::GatewayIntents};
 
 use serenity::model::gateway::Ready;
 
 mod events;
 
-pub struct Handler;
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    groups: Vec<Group>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Group {
+    channels: Vec<String>,
+    keywords: Vec<Keyword>,
+    regex: Vec<Regex>,
+    parsing: Parsing
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Keyword {
+    keyword: String,
+    reaction: Option<String>,
+    response: String
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Regex {
+    pattern: String,
+    reaction: Option<String>,
+    response: String
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Parsing {
+    paste: Vec<Paste>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Paste {
+    domain: String,
+    format: String
+}
+
+pub struct Handler {
+    pub cfg: Config,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -16,6 +57,7 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: serenity::model::channel::Message) {
+        if msg.author.bot { return; }
         events::message::message(self, ctx, msg).await;
     }
 }
@@ -24,10 +66,16 @@ impl EventHandler for Handler {
 async fn main() {
     dotenv::dotenv().expect("Failed to read .env file");
 
+    let file = File::open("./config.json").expect("Failed to open config file");
+    let reader = BufReader::new(file);
+    let cfg: Config = serde_json::from_reader(reader).expect("Failed to parse config file");
+
+    let handler = Handler { cfg };
+
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&env::var("DISCORD_TOKEN").expect("Expected a token"), intents)
-        .event_handler(Handler)
+        .event_handler(handler)
         .await
         .expect("Err creating client");
 
